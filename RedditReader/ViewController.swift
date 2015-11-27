3ee489d4d33dac001eb6132ca3036816f96ced47
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2015-11-27 19:30:15 +0530

    Initial Commit

---
+++ RedditReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditReader
+//
+//  Created by Vagmi on 27/11/15.
+//  Copyright © 2015 Vagmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
